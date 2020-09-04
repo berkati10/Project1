b5ced7507dfb17b0d108e4649bdcabe93d00a296
anasberkati <anasberkati@gmail.com>
2020-09-04 15:45:02 +0200

    Ajout du Menu avec Props

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = ({title}) => {
+    return (
+        <div>
+            Ceci est un MENU, il s'appelle {title}
+        </div>
+    )
+}
+
+export default Navbar
